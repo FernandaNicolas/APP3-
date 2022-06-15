3748582d0a636096669e485297f418712d9aac67
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2022-06-15 10:13:27 -0400

    ver1

--- Main.js
+++ Main.js
@@ -1,11 +1,11 @@
-var _ = require("underscore");
+//var _ = require("underscore");
 
 // Se marcará el inicio del laberinto con un 2
 function marcar_inicio(tablero){
     let flag = true;
     tablero = tablero.forEach(function(tabla){
-        if(flag === true && tablero[tabla][0]=== 0){
-            tablero[tabla][0] = 2;
+        if(flag === true && tabla[0]=== 0){
+            tabla[0] = 2;
             flag = false;
         }
     });
@@ -15,12 +15,19 @@ function marcar_inicio(tablero){
 function  marcar_final(tablero){
     let flag = true;
     tablero = tablero.forEach(function(tabla){
-        if(flag === true && tablero[tabla][tabla.length-1]=== 0){
-            tablero[tabla][tabla.length-1] = 3;
+        if(flag === true && tabla[tabla.length-1]=== 0){
+            tabla[tabla.length-1] = 3;
             flag = false;
         }
     });
     return tablero;
 }
 
-let laberinto = [[],[],[],[],[]]
+let laberinto = [[1,0,1,0,1],
+                 [1,0,1,0,1],
+                 [0,0,0,0,0],
+                 [1,0,1,0,0],
+                 [0,0,0,0,1]]
+console.log(marcar_inicio(laberinto));
+console.log(marcar_final(laberinto));
+console.log(laberinto);
