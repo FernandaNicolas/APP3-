481e4309788ab0672f941e65c7cea999edb10e12
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2022-06-18 17:08:39 -0400

    listos para comenzar función recursiva

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // Se marcará el inicio del laberinto con un 2
 function marcar_inicio(tablero){
     let flag = true;
-    tablero = tablero.forEach(function(tabla){
+    tablero.forEach(function(tabla){
         if(flag === true && tabla[0]=== 0){
             tabla[0] = 2;
             flag = false;
@@ -14,7 +14,7 @@ function marcar_inicio(tablero){
 // Se marcará el final del laberinto con un 3
 function  marcar_final(tablero){
     let flag = true;
-    tablero = tablero.forEach(function(tabla){
+    tablero.forEach(function(tabla){
         if(flag === true && tabla[tabla.length-1]=== 0){
             tabla[tabla.length-1] = 3;
             flag = false;
@@ -22,20 +22,16 @@ function  marcar_final(tablero){
     });
     return tablero;
 }
-
-/*let laberinto = [[1,0,1,0,1],
-                   [1,0,1,0,1],
-                   [0,0,0,0,0],
-                   [1,0,1,0,0],
-                   [0,0,0,0,1]];
-console.log(marcar_inicio(laberinto));
-console.log(marcar_final(laberinto));
-*/
-fs.readFile('input.txt', 'utf8',(err,data)=>{
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log(data);
+// Ahora leeremos el archivo "input.txt" y lo transformamos en un array de arrays
+const log = data => console.log(JSON.stringify(data));
+let txt = fs.readFileSync('input.txt', 'utf8',);
+let lines  = txt.trim().split(/\n/).map(row => row.split(/\s/));
+lines[lines.length-1].push("");
+let laberinto = lines.map(row => row.map(bit => +bit));
+laberinto.forEach(function (pos){
+    pos.pop(pos.length-1);
 });
-//console.log(laberinto);
+log(laberinto);
+laberinto = marcar_inicio(laberinto);
+laberinto = marcar_final(laberinto)
+console.log(laberinto);
